4de20c80b8e9b0d439dbd4fc71389a5a92e27668
giuse-pio <giupeppepio7@gmail.com>
2025-04-25 20:25:56 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace _22._26
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConv = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lstConv = new System.Windows.Forms.ListBox();
+            this.txtNum = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnConv
+            // 
+            this.btnConv.Location = new System.Drawing.Point(160, 131);
+            this.btnConv.Name = "btnConv";
+            this.btnConv.Size = new System.Drawing.Size(125, 42);
+            this.btnConv.TabIndex = 0;
+            this.btnConv.Text = "converti in testo";
+            this.btnConv.UseVisualStyleBackColor = true;
+            this.btnConv.Click += new System.EventHandler(this.btnConv_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(160, 200);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(131, 39);
+            this.btnReset.TabIndex = 1;
+            this.btnReset.Text = "reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(196, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "numero";
+            // 
+            // lstConv
+            // 
+            this.lstConv.FormattingEnabled = true;
+            this.lstConv.Location = new System.Drawing.Point(410, 81);
+            this.lstConv.Name = "lstConv";
+            this.lstConv.Size = new System.Drawing.Size(174, 82);
+            this.lstConv.TabIndex = 3;
+            // 
+            // txtNum
+            // 
+            this.txtNum.Location = new System.Drawing.Point(160, 95);
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(116, 20);
+            this.txtNum.TabIndex = 4;
+            this.txtNum.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNum_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtNum);
+            this.Controls.Add(this.lstConv);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnConv);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConv;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lstConv;
+        private System.Windows.Forms.TextBox txtNum;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _22._26
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtNum_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 8)
+            {
+                return;
+            }
+            else if (e.KeyChar == 13)
+            {
+                btnConv_Click(sender, e);
+                e.Handled = true;
+            }
+            else if (char.IsDigit(e.KeyChar))
+            {
+                return;
+            }
+            else
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void btnConv_Click(object sender, EventArgs e)
+        {
+            if (txtNum.Text == "")
+            {
+                return;
+            }
+            var numConv = converti(Convert.ToInt32(txtNum.Text));
+            lstConv.Items.Add(txtNum.Text + "\t" + numConv);
+            Console.WriteLine(txtNum.Text + "\t" + numConv);
+            txtNum.Text = "";
+            txtNum.Focus();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            txtNum.Text = "";
+            lstConv.Items.Clear();
+            txtNum.Focus();
+        }
+        string converti(int num)
+        {
+            List<int> cifre = new List<int>();
+            int numConv = num;
+            while (numConv > 0)
+            {
+                int cifra = numConv % 10;
+                numConv = numConv / 10;
+                cifre.Add(cifra);
+            }
+            string milioni;
+            switch (cifre.Count)
+            {
+                case 1:
+                    return convDU(0, cifre[0]);
+                case 2:
+                    return convDU(cifre[1], cifre[0]);
+                case 3:
+                    return convCDU(cifre[2], cifre[1], cifre[0]);
+                case 4:
+                    return
+                        convM(0, 0, cifre[3]) +
+                        convCDU(cifre[2], cifre[1], cifre[0]);
+                case 5:
+                    return
+                        convM(0, cifre[4], cifre[3]) +
+                        convCDU(cifre[2], cifre[1], cifre[0]);
+                case 6:
+                    return
+                        convM(cifre[5], cifre[4], cifre[3]) +
+                        convCDU(cifre[2], cifre[1], cifre[0]);
+                case 7:
+                    if (cifre[6] == 1)
+                        milioni = "un milione";
+                    else
+                        milioni = convDU(0, cifre[6]) + " milioni";
+                    return
+                        milioni +
+                        convM(cifre[5], cifre[4], cifre[3]) +
+                        convCDU(cifre[2], cifre[1], cifre[0]);
+                case 8:
+                    milioni = convDU(cifre[7], cifre[6]) + " milioni";
+                    return
+                        milioni +
+                        convM(cifre[5], cifre[4], cifre[3]) +
+                        convCDU(cifre[2], cifre[1], cifre[0]);
+                default:
+                    return "";
+            }
+        }
+        string convDU(int dec, int uni)
+        {
+            string[] testiNum0_19 = { "zero", "uno", "due", "tre", "quattro", "cinque",
+                "sei", "sette", "otto", "nove", "dieci", "undici", "dodici", "tredici",
+                "quattordici", "quindici", "sedici", "diciassette", "diciotto", "diciannove" };
+            string[] testiDecine = { "venti", "trenta", "quaranta", "cinquanta", "sessanta",
+                "settanta", "ottanta", "novanta" };
+            int valore = dec * 10 + uni;
+            if (valore < 20)
+            {
+                return testiNum0_19[valore];
+            }
+            else if (uni == 1 || uni == 8)
+            {
+                return testiDecine[dec].Substring(0, testiDecine[dec].Length - 1) + testiNum0_19[uni];
+            }
+            else
+            {
+                return testiDecine[dec] + testiNum0_19[uni];
+            }
+        }
+        string convCDU(int cent, int dec, int uni)
+        {
+            string[] testiCentinaia = { "cento", "duecento", "trecento", "quattrocento",
+                "cinquecento", "seicento", "settecento", "ottocento", "novecento" };
+            return testiCentinaia[cent] +
+                convDU(dec, uni);
+        }
+        string convM(int cent, int dec, int uni)
+        {
+            if (cent == 0 && dec == 0 && uni == 0)
+                return "";
+            else if (cent == 0 && dec == 0 && uni == 1)
+                return "mille";
+            else
+                return convCDU(cent, dec, uni) + " mila";
+        }
+    }
+}
